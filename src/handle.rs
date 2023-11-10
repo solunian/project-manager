5efc3d47d777d9bfb8263073545a6cfe36545535
Daniel Cui <solunian@gmail.com>
2023-11-09 19:20:56 -0800

    feat: group args to be more processible | build: serde, yaml libs

--- src/handle.rs
+++ src/handle.rs
@@ -1,15 +1,38 @@
 use std::{fs, env};
 
+const VERSION: &str = "0.0.1";
 const DOT_DIR: &str = "./.tart";
 
-pub fn init(args: &[String]) -> String {
+pub type Message = String;
+
+
+pub fn tart(args: &[String]) -> Message {
+  let grouped_args = group_args(args);
+
+  for group in grouped_args {
+     // should match group[0] with options or main commands
+     // then loop through remaining values in group for certain
+    match group[0] {
+      "-v" | "--version" => {
+         return about_tart();
+      },
+      _ => {
+        return unknown();
+      }
+    }
+  }
+  
+  help(args) // base function of command without args/opts
+}
+
+pub fn init(args: &[String]) -> Message {
   match args.len() {
     1 => init_dot_dir(),
-    _ => format!("unknown option")
+    _ => unknown()
   }
 }
 
-pub fn destroy(args: &[String]) -> String {
+pub fn destroy(args: &[String]) -> Message {
   match args.len() {
     1 => {
       match fs::remove_dir_all(DOT_DIR) {
@@ -17,12 +40,30 @@ pub fn destroy(args: &[String]) -> String {
         Err(_) => format!("Failed to destroy .tart in {}", get_current_dir())
       }
     },
-    _ => format!("unknown option")
+    _ => unknown()
   }
-  
 }
 
-fn init_dot_dir() -> String {
+pub fn create(args: &[String]) -> Message {
+  match args.len() {
+    2 => {
+      format!("Created project {}", args[1])
+    },
+    _ => unknown()
+  }
+}
+
+pub fn help(_args: &[String]) -> Message {
+"tart: a simple command-line project manager
+  
+work in progress!".to_string()
+}
+
+fn about_tart() -> Message {
+  format!("tart v{}", VERSION)
+}
+
+fn init_dot_dir() -> Message {
   match fs::create_dir(DOT_DIR) {
     Ok(()) => format!("Initialized tart in {}/.tart", get_current_dir()),
     Err(_) => "Failed to create directory".to_string()
@@ -31,4 +72,38 @@ fn init_dot_dir() -> String {
 
 fn get_current_dir() -> String {
   env::current_dir().unwrap().into_os_string().into_string().unwrap()
+}
+
+fn unknown() -> Message {
+  "unknown command or option".to_string()
+}
+
+// separates args into `-` | `--` and the following non-opt args on one Vec each
+fn group_args(args: &[String]) -> Vec<Vec<&str>> {
+  let mut new_args: Vec<Vec<&str>> = Vec::new();
+  let mut subvec_idx: usize = 0;
+
+  for arg in args {
+    if arg.starts_with("--") {
+      subvec_idx += 1;
+      new_args.push(vec![arg]);
+    } else if arg.starts_with("-") {
+      subvec_idx += 1;
+      if arg.len() == 2 { // normal opt
+        new_args.push(vec![&arg[0..2]]);
+      } else { // for one-letter opt with the body tacked onto it
+        new_args.push(vec![&arg[0..2], &arg[2..]]);
+      }
+    } else {
+      // for when there are no current Vec in new_args
+      if new_args.len() == subvec_idx {
+        new_args.push(Vec::new());
+      }
+
+      // all non-opt get added to the newest Vec
+      new_args[subvec_idx].push(arg);
+    }
+  }
+
+  new_args
 }
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,61 @@
 use std::env;
 use colored::Colorize;
 
+use crate::handle::Message;
+
 mod handle;
 
-const VERSION: &str = "0.0.1";
 const DEV_MODE: bool = true;
 
 fn main() {
-    // vector includes executable as first, then args
-    let exec_args: Vec<String> = env::args().collect();
+  // Vec includes executable as first, then args
+  let exec_args: Vec<String> = env::args().collect();
 
 
-    // DEV STUFF
-    if DEV_MODE {
-        println!("{}", "\n=======================================================".blue().bold());
-        println!(        "Command Line `exec_args` (each value surrounded by < >)");
-        for i in exec_args.iter() {
-            print!("<{}> ", i.purple().bold());
-        }
-        println!("{}", "\n=======================================================".blue().bold());
+  // DEV STUFF
+  if DEV_MODE {
+    println!("{}", "\n=======================================================".blue().bold());
+    println!(        "Command Line `exec_args` (each value surrounded by < >)");
+    for i in exec_args.iter() {
+      print!("<{}> ", i.purple().bold());
     }
-    // ===========================
+    println!("{}", "\n=======================================================".blue().bold());
+  }
+  // ===========================
 
 
-    let args: &[String] = &exec_args[1..];
-    
-    if args.len() == 0 {
-        println!("\ntart - a project manager\n");
-        return;
-    }
-    
+  let args: &[String] = &exec_args[1..];
+  let mut message: Message = handle::help(args);
+  
+  if args.len() > 0 {
     let command: &str = &args[0];
 
-    let message = match command {
-        "init" => {
-            handle::init(args)
-        },
-        "destroy" => {
-            handle::destroy(args)
-        }
-        "create" => {
-            "create".to_string()
-        }
-        "status" => {
-            "status".to_string()
-        },
-        "stats" => {
-            "stats".to_string()
-        },
-        "help" => {
-            "help".to_string()
-        }
-        _ => {
-            match command {
-                "-v" | "--version" => {
-                    format!("project-manager v{}", VERSION)
-                },
-                _ => {
-                    "'{}' is not a tart command. See 'tart help'.".to_string()
-                }
-            }
-        }
+    message = match command {
+      "init" => {
+        handle::init(args)
+      },
+      "destroy" => {
+        handle::destroy(args)
+      }
+      "create" => {
+        handle::create(args)
+      }
+      "status" => {
+        "status not implemented yet".to_string()
+      },
+      "stats" => {
+        "stats not implemented yet".to_string()
+      },
+      "help" => {
+        handle::help(args)
+      }
+      _ => {
+        handle::tart(args)
+      }
     };
 
-    println!("\n{}\n", message); // \n for extra space
+  }
+  
+  println!("\n{}\n", message); // \n for extra space
 
 }
