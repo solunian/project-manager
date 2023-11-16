2d5193647d7f6db3bf9ce1b0e945ccf93f7fe857
Daniel Cui <solunian@gmail.com>
2023-11-16 01:34:24 -0800

    refactor: organize handle funcs

--- src/handle.rs
+++ src/handle.rs
@@ -13,44 +13,52 @@ pub fn destroy(dir: Option<&PathBuf>) -> Message {
   destroy_dot_dir(dir)
 }
 
-// TODO: check if dir already exists
 fn init_dot_dir(dir: Option<&PathBuf>) -> Message {
-  let mut actual_path = env::current_dir().unwrap().join(DOT_DIR);
+  let mut actual_path = env::current_dir().unwrap();
   if let Some(given_dir) = dir {
-    actual_path = given_dir.join(".tart");
+    actual_path = given_dir.to_path_buf();
   }
+  actual_path.push(DOT_DIR);
 
-  // match fs::canonicalize(&actual_path) { // check if path exists
-  //   Ok(path) => {
-      
-  //   },
-  //   Err(_) => format!("Path `{}` not found", &actual_path.to_str().unwrap())
-  // }
+  if actual_path.exists() {
+    return format!("Path `{}` already exists", &actual_path.to_str().unwrap());
+  }
 
   match fs::create_dir(&actual_path) { // attempts to init dir
-    Ok(()) => format!("Initialized tart in {}", fs::canonicalize(&actual_path).unwrap().to_str().unwrap()),
-    Err(_) => format!("Failed to initialize tart in {}", &actual_path.to_str().unwrap())
+    Ok(()) => format!("Initialized tart in {}", get_absolute_path(&actual_path).unwrap()), // path should exist after create_dir
+    Err(_) => format!("Failed to initialize tart in {}", &actual_path.to_str().unwrap()) // path does not exist
   }
 }
-
 
 fn destroy_dot_dir(dir: Option<&PathBuf>) -> Message {
-  let mut actual_path = env::current_dir().unwrap().join(DOT_DIR);
+  let mut actual_path = env::current_dir().unwrap();
   if let Some(given_dir) = dir {
-    actual_path = given_dir.join(".tart");
+    actual_path = given_dir.to_path_buf();
   }
+  actual_path.push(DOT_DIR);
 
-  match fs::canonicalize(&actual_path) { // check if path exists
-    Ok(path) => {
+  match get_absolute_path(&actual_path) {
+    Ok(path) => { // path exists 
       match fs::remove_dir_all(&path) { // attempts to remove dir
-        Ok(()) => format!("Destroyed tart in {}", &path.to_str().unwrap()),
-        Err(_) => format!("Failed to destroy tart in {}", &path.to_str().unwrap())
+        Ok(()) => format!("Destroyed tart in {}", &path),
+        Err(_) => format!("Failed to destroy tart in {}", &path)
       }
     },
-    Err(_) => format!("Path `{}` not found!", &actual_path.to_str().unwrap())
+    Err(err_message) => err_message // path does not exist
   }
 }
 
-fn _get_current_dir() -> String {
-  env::current_dir().unwrap().into_os_string().into_string().unwrap()
+fn get_absolute_path(path: &PathBuf) -> Result<Message, Message> {  
+  match fs::canonicalize(&path) {
+    Ok(pathbuf) => {
+      let mut path_str = pathbuf.to_str().unwrap();
+
+      if cfg!(windows) {
+        path_str = path_str.trim_start_matches(r"\\?\");
+      }
+
+      Ok(path_str.to_string())
+    },
+    Err(_) => Err(format!("Path `{}` not found", &path.to_str().unwrap()))
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use clap::{Parser, Subcommand};
 mod handle;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about=None, arg_required_else_help=true)]
+#[command(author, version, long_about=None, arg_required_else_help=true)]
 struct CLI {
   #[command(subcommand)]
   command: Option<Commands>,
@@ -20,7 +20,6 @@ enum Commands {
   #[command()]
   /// Initialize tart
   Init {
-    /// Path of tart initialization.
     directory: Option<PathBuf>,
   },
   #[command()]
@@ -43,5 +42,5 @@ fn main() {
       println!("{}", handle::destroy(directory.as_ref()));
     },
     None => {}
-  }
+  };
 }
